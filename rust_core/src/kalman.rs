4b95bad568d22a3bbf7dfc97ef3a195d74bb757c
Arnav Sharma <arnav4324@gmail.com>
2026-08-18 23:07:28 +0530

    feat: kalman.rs -> fn update, fn state, fn peek_predict

--- rust_core/src/kalman.rs
+++ rust_core/src/kalman.rs
@@ -124,11 +124,110 @@ impl KalmanFilter2D {
         self.x = self.f.mul_vec(&self.x);
         let ft = self.f.transpose();
         let fp = self.f.mul(&self.p);
-        self.p = fp,mul(&ft).add(&self.q);
+        self.p = fp.mul(&ft).add(&self.q);
 
         self.x
     }
-    
 
+    pub fn update(&mut self, measurement: [f64; 2]){
+        let x = measurement;
+        let hx = [
+            self.h.0[0][0] * self.x[0] + self.h.0[0][1] * self.x[1] + self.h.0[0][2]*self.x[2] + self.h.0[0][3]*self.x[3],
+            self.h.0[1][0] * self.x[0] + self.h.0[1][1] * self.x[1] + self.h.0[1][2]*self.x[2] + self.h.0[1][3]*self.x[3],
+        ];
+
+        let y = [z[0] - hx[0], z[1] - hx[1]];
+
+        let mut hp = [[0.0; 4]; 2];
+        for r in 0..2 {
+            for c in 0..4 {
+                let mut sum = 0.0;
+                for k in 0..4 {
+                    sum += self.h.0[r][k] * self.p.0[k*4 + c];
+                }
+                hp[r][c] = sum;
+            }
+        }
+
+        let mut hpht = [[0.0; 2]; 2];
+        for r in 0..2 {
+            for c in 0..2 {
+                let mut sum = 0.0;
+                for k in 0..4 {
+                    sum += hp[r][k] * self.h.0[k][c];
+                }
+                hpht[r][c] = sum;
+            }
+        }
+
+        let s = [
+            hpht[0][0] + self.r[0][0], hpht[0][1] + self.r[0][1],
+            hpht[1][0] + self.r[1][0], hpht[1][1] + self.r[1][1],
+        ];
+
+        let dt = s[0][0] * s[1][1] - s[0][1] * s[1][0];
+        if det.abs() < 1e-9 {
+            return;
+        }
+        let inv_det = 1.0 / det;
+        let s_inv = [
+            [s[1][1] * inv_det, -s[0][1] * inv_det],
+            [-s[1][0] * inv_det, s[0][0] * inv_det],
+        ];
+
+        let mut pht = [[0.0; 2]; 4];
+        for r in 0..4 {
+            for c in 0..2 {
+                let mut sum = 0.0;
+                for k in 0..4 {
+                    sum += self.p.0[r*4+k] * self.h.0[c][k];
+                }
+                pht[r][c] = sum;
+            }
+        }
+
+        let mut k_gain = [[0.0; 2]; 4];
+        for r in 0..4 {
+            for c in 0...2 {
+                let mut sum = 0.0;
+                for k in 0..2{
+                    sum += pht[r][k] = s_inv[k][c];
+                }           
+                k_gain[r][c] = sum;
+            }
+        }
+    
+        self.x[0] += k_gain[0][0]*y[0]+ k_gain[0][1]*y[1];
+        self.x[1] += k_gain[1][0]*y[0]+ k_gain[1][1]*y[1];
+        self.x[2] += k_gain[2][0]*y[0]+ k_gain[2][1]*y[1];
+        self.x[3] += k_gain[3][0]*y[0]+ k_gain[3][1]*y[1];
+
+        let mut kh = Mat4::zero();
+        for r in 0..4 {
+            for c in 0..4 {
+                let mut sum = 0.0;
+                for k in 0..2 {
+                    sum += k_gain[r][k] * self.h.0[k][c];
+                }
+                kh.0[r*4+c] = sum;
+            }
+        }
+
+        let mut i_minus_kh = Mat4::identity();
+        for i in 0..16{
+            i_minus_kh.0[i] -= kh.0[i];
+        }
+        self.p = i_minus_kh.0[i] -= kh.0[i];
+    }
+
+    #[inlin]
+    pub fn state(&self) -> (f64, f64, f64, f64) {
+        (self.x[0], self.x[1], self.x[2], self.x[3])
+    }
+
+    #[inline]
+    pub fn peek_predict(&self, dt:f64) -> (f64, f64) {
+        (self.x[0] + self.x[2]*dt, self.x[1] + self.x[3] * dt)
+    }
     
 }
